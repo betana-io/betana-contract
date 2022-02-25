dcb4519b87506eb5b2e31a2bc8f7bb65ed6bbbb0
Teddy Jean-Francois <teddy.jean-francois@ibm.com>
2022-02-25 15:37:21 +0100

    feat : initialize instruction in process

--- programs/betana/src/processor.rs
+++ programs/betana/src/processor.rs
@@ -20,6 +20,34 @@ use {
 pub struct Processor;
 impl Processor {
 
+    /// Processes [Instruction]
+    ///
+    /// 0. `[]` Program id of the currently executing program
+    /// 1. `[]` Account because Solana programs are stateless
+    /// 2. `[]` Data passed to the program by the caller, it could be anything
+    pub fn process(program_id: &Pubkey, accounts: &[AccountInfo], instruction_data: input: &[u8]) -> ProgramResult {
+        // Deserialize this instance from a slice of bytes.
+        // Unpacks a byte buffer into a [StakePoolInstruction](enum.StakePoolInstruction.html).
+        let instruction = StakePoolInstruction::try_from_slice(input)?;
+        match instruction {
+            StakePoolInstruction::InitializePool => {
+                msg!("Instruction: InitializePool");
+                Self::create_stake_pool()
+            }
+            StakePoolInstruction::DepositBet => {
+                msg!("Instruction: DepositBet");
+                Self::deposit_bet()
+            }
+            StakePoolInstruction::CalculateRewards => {
+                msg!("Instruction: CalculateRewards");
+                Self::withdraw_rewards()
+            }
+            StakePoolInstruction::GetPoolBalance => {
+                msg!("Instruction: GetPoolBalance");
+                Self::get_stake_pool_balance()
+            }
+        }
+    }
 
 }
 
