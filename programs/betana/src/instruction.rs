0ee75e8a6430499a0a59598cadf96782843393db
Teddy Jean-Francois <teddy.jean-francois@ibm.com>
2022-03-06 19:21:46 +0100

    feat : add deposit bet instruction

--- programs/betana/src/instruction.rs
+++ programs/betana/src/instruction.rs
@@ -24,6 +24,7 @@ pub struct Fee {
 #[repr(C)]
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct InitArgs {
+    /// withdraw and deposit fees can be add
     /// Fee paid to the owner in pool tokens
     pub fee: Fee,
 }
@@ -33,14 +34,29 @@ pub struct InitArgs {
 pub enum StakePoolInstruction {
     /// Initializes a new StakePool, create 3 pools - Pool A (team 1) / Pool Draw / Pool B (team 2)
     ///
-    /// 0. `[]` Identifier of the match
+    /// 0. `[w]` New StakePool to create.
+    /// 1. `[s]` Owner / Manager
+    /// 2. `[w]` Uninitialized validator stake list storage account
+    /// 3. `[]` Pool token Mint. Must be non zero, owned by withdraw authority.
+    /// 4. `[]` Pool account to deposit the generated fee for owner / manager.
+    /// 5. `[]` Token program id
+    /// 6. `[]` Identifier of the match
     Initialize(InitArgs),
 
     /// Add a bet in the common match pool.
     ///
-    /// 0. `[]` Amount that you want to bet for the match
-    /// 1. `[]` Identifier of the match
-    /// 2. `[]` Team that will win the match
+    /// 0. `[w]` Stake pool
+    /// 1. `[w]` Validator stake list storage account
+    /// 2. `[w]` Validator stake account for the stake account to be merged with
+    /// 3. `[w]` Reserve stake account, to withdraw rent exempt reserve
+    /// 4. `[w]` User account to receive pool tokens
+    /// 5. `[w]` Account to receive pool fee tokens
+    /// 6. `[w]` Account to receive a portion of pool fee tokens as referral fees
+    /// 7. `[w]` Pool token mint account
+    /// 8. `[]` Pool token program id,
+    /// 9. `[]` Amount that you want to bet for the match
+    /// 10. `[]` Identifier of the match
+    /// 11. `[]` Team that will win the match
     DepositBet,
 
     /// Calculate the different rewards for a match 
@@ -119,6 +135,7 @@ pub fn initialize(
     manager_pool_account: &Pubkey,
     token_program_id: &Pubkey,
     init_args: InitArgs,
+    // match_id
 ) -> Result<Instruction, ProgramError> {
     let init_data = StakePoolInstruction::Initialize(init_args);
     let data = init_data.serialize()?;
@@ -126,6 +143,48 @@ pub fn initialize(
         AccountMeta::new(*stake_pool, false),
         AccountMeta::new_readonly(*manager, true),
         AccountMeta::new(*validator_stake_list, false),
+        AccountMeta::new(*pool_mint, false),
+        AccountMeta::new_readonly(*token_program_id, false),
+    ];
+
+    Ok(Instruction {
+        program_id: *program_id,
+        accounts,
+        data,
+    })
+}
+
+pub fn deposit_bet(
+    program_id: &Pubkey,
+    stake_pool: &Pubkey,
+    validator_stake_list: &Pubkey,
+    stake_pool_deposit: &Pubkey,
+    stake_pool_withdraw: &Pubkey,
+    stake_account_join: &Pubkey,
+    validator_stake_account: &Pubkey,
+    reserve_stake_account: &Pubkey,
+    pool_tokens_to: &Pubkey,
+    manager_fee_account: &Pubkey,
+    referrer_pool_tokens_account: &Pubkey,
+    pool_mint: &Pubkey,
+    token_program_id: &Pubkey,
+    // bet
+    // match_id
+    // pick
+) -> Result<Instruction, ProgramError> {
+    let args = StakePoolInstruction::DepositBet;
+    let data = args.serialize()?;
+    let accounts = vec![
+        AccountMeta::new(*stake_pool, false),
+        AccountMeta::new(*validator_stake_list, false),
+        AccountMeta::new_readonly(*stake_pool_deposit, false),
+        AccountMeta::new_readonly(*stake_pool_withdraw, false),
+        AccountMeta::new(*stake_account_join, false),
+        AccountMeta::new(*validator_stake_account, false),
+        AccountMeta::new(*reserve_stake_account, false),
+        AccountMeta::new(*pool_tokens_to, false),
+        AccountMeta::new(*manager_fee_account, false),
+        AccountMeta::new(*referrer_pool_tokens_account, false),
         AccountMeta::new(*pool_mint, false),
         AccountMeta::new_readonly(*token_program_id, false),
     ];
